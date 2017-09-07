163c0ccee4a0e0a8c9ef7a9ef38e85be97ada8b8
lihewang <lihe_wang@hotmail.com>
2017-09-07 09:43:12 -0400

    Delete pop.lua

--- pop.lua
+++
@@ -1,7 +0,0 @@
---pop job
-redis.call("select",KEYS[1])
-local z = redis.call("lpop",KEYS[2])
-if (type(z) == "boolean" and not z) or z == "done" then
- redis.call("rpush", KEYS[2], "done")
-end
-return z
