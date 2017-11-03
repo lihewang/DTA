64eb902102131bbc92752bfd79f7fa7135910cd2
lihewang <lihe_wang@hotmail.com>
2017-11-03 16:18:42 -0400

    Add files via upload

--- msa.lua
+++ msa.lua
@@ -1,11 +1,15 @@
 --msa volume
 redis.call("select","2")
 local z = redis.call("exists",KEYS[1])
+local y = redis.call("exists",KEYS[2])
+local vc = 0
+local vp = 0
 if z == 1 then
- local vc = redis.call("get",KEYS[1])
- local vp = redis.call("get",KEYS[2])
- local v = vp*(1-1/KEYS[3])+vc/KEYS[3]
- redis.call("set",KEYS[1],v)
- return vc .. "," .. vp .. "," .. v
+ vc = redis.call("get",KEYS[1])
 end
-return "0,0,0"
+if y == 1 then
+ vp = redis.call("get",KEYS[2])
+end
+local v = vp*(1-KEYS[3])+vc*KEYS[3]
+redis.call("set",KEYS[1],v)
+return vc .. "," .. vp .. "," .. v
