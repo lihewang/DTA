5b4b42c9f8fcaec1bf1409af1f858724c2e96906
lihewang <lihe_wang@hotmail.com>
2018-03-07 16:45:35 -0500

    Add files via upload

--- run.js
+++ run.js
@@ -2,13 +2,13 @@ var async = require('async');
 var google = require('googleapis');
 var container = google.container('v1');
 var K8s = require('k8s');
-//var stringify = require('json-stringify');
 
-var prjId = "dta-01";
+var prjId = "dta-beta";
 var zone = "us-central1-a";
 var clusterName = "eltod";
-var key = require('./dta-01-7b4aaca7da3d.json'); //service account key
-
+var kubectlBin = 'C:\\Users\\lihe.wang\\AppData\\Local\\Google\\Cloud SDK\\google-cloud-sdk\\bin\\kubectl.exe';
+var key = require('./dta-beta-c31385fcfaac.json'); //service account key
+var endPnt = '35.226.114.170';
 //clear console
 process.stdout.write('\033c');
 
@@ -26,8 +26,9 @@ jwtClient.authorize(function (err) {
         console.log('authorize err ' + err);
     } else {
         console.log('access to google cloud authorized');
-        createCluster(); 
+        //createCluster();
         //deployRedis();
+        deployWorker();
     }
 });
 
@@ -40,6 +41,9 @@ var createCluster = function () {
             cluster: {
                 name: clusterName,
                 initialNodeCount: "3",
+                nodeConfig: {
+                    "machineType": "f1-micro"
+                }
             }
         },
         auth: jwtClient
@@ -49,26 +53,30 @@ var createCluster = function () {
         if (err) {
             console.log('create cluster err ' + err);
         } else {
-            console.log('creat cluster');
             var request = {
                 projectId: prjId,
                 zone: zone,
                 clusterId: clusterName,
-                fields: "status",
                 auth: jwtClient
             };
+            var dots = '';
             //wait until the cluster is running
-            objInterval = setInterval(function () {
+            objInterval = setInterval(function () {                
                 container.projects.zones.clusters.get(request, function (err, res) {
                     if (err) {
                         console.log('get cluster info err ' + err);
                     } else {
                         if (res.data.status == 'RUNNING') {
-                            console.log('cluster is running');
+                            endPnt = res.data.endpoint;
+                            console.log('');
+                            console.log('cluster is running at ' + endPnt);
                             clearInterval(objInterval);
                             deployRedis();
                         } else {
-                            console.log('waiting for cluster to be ready ...');
+                            dots = dots + '.';
+                            process.stdout.clearLine();
+                            process.stdout.cursorTo(0); 
+                            process.stdout.write('creating cluster ' + dots);
                         }
                     }
                 });
@@ -78,9 +86,60 @@ var createCluster = function () {
 }    
 
 var deployRedis = function () {
-    console.log('creating redis ');
+    console.log('deploy redis');
+    var kubectl = K8s.kubectl({
+        endpoint: 'https://' + endPnt,
+        binary: kubectlBin
+    })
+    kubectl.command('create -f redis.yaml', function (err, result) {
+        if (err) {
+            console.log('create redis ' + err);
+        }
+        console.log('create redis ' + result);
+        kubectl.command('expose redis --port 6379', function (err, result) {
+            if (err) {
+                console.log('expose redis ' + err);
+            }
+            console.log('expose redis - ' + result);
+            deployWorker();
+        });
+    });    
 }
 
+var deployWorker = function () {
+    console.log('deploy workers');
+    var kubectl = K8s.kubectl({
+        endpoint: 'https://' + endPnt,
+        binary: kubectlBin
+    })
+    //wait until redis is running
+    var dots = '';
+    objInterval2 = setInterval(function () {
+        kubectl.command('get service -o jsonpath="{.items[?(@.metadata.name==\'redis\')].metadata.name}"', function (err, result) {
+            if (err) {
+                console.log('get service ' + err);
+            } else {                
+                if (result == '"redis"') {
+                    clearInterval(objInterval2);
+                    console.log('redis deployed');
+                    kubectl.command('create -f worker.yaml', function (err, result) {
+                        if (err) {
+                            console.log('create workers ' + err);
+                        }
+                        console.log('create workers - ' + result);
+                    });
+                } else {
+                    dots = dots + '.';
+                    process.stdout.clearLine();
+                    process.stdout.cursorTo(0);
+                    process.stdout.write('creating redis ' + dots);
+                }
+            }
+        });
+    }, 5000);                
+    ////workers
+    
+}
 //var deployWorker = setInterval(function () {
 //    console.log('creating redis ');
 //    //process.exit();
